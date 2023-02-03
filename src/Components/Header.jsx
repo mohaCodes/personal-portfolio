c5153533b71292a9292e4055e4f7d7f80868d6d1
Mohamed Abdulmueen <moha.abdulmueen@gmail.com>
2023-02-03 17:47:20 +0300

    Changed h1 tag to h3

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,9 +3,9 @@ import React from "react"
 function Header() {
     return (
         <nav className="border border-red-500 text-xl px-6 py-5 font-lato text-white-text">
-            <h1 className="">
+            <h3 className="">
                 Mohamed <sub className="text-[0.6rem]">A. Mueen</sub>
-            </h1>
+            </h3>
         </nav>
     )
 }
