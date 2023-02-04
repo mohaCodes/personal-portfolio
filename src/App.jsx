072b4b233fe42e105c476e3c738d9012cfb646df
Mohamed Abdulmueen <moha.abdulmueen@gmail.com>
2023-02-04 23:41:56 +0300

    added import skills.js statement

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import Nav from "./Components/Nav"
 import PersonalInfo from "./Components/personalInfo"
 import About from "./Components/About"
 
+// importing skills array
+import skills from "../skills"
+
 function App() {
     return (
         <div className="">
             <Nav />
             <PersonalInfo />
             <About />
+            
         </div>
     )
 }
