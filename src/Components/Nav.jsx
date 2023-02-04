c51642d976322e31fc2360fdf6d8bb4793f69f66
Mohamed Abdulmueen <moha.abdulmueen@gmail.com>
2023-02-04 12:56:56 +0300

    Created component and done tweaking it

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+export default function Nav() {
+    return (
+        //This is the nav element
+        <nav className="sticky">
+            <div className="flex flex-row w-full border border-white p-5 align-baseline justify-between text-[#D9C7BD]">
+                <div className="flex flex-row text-xs ">
+                    <span className="material-symbols-rounded self-center text-xl">
+                        radio_button_unchecked
+                    </span>
+                    <div className="ml-1.5 font-medium ">
+                        <h3 className="">Mohamed</h3>
+                        <h3 className=" -mt-1">A. Mueen</h3>
+                    </div>
+                </div>
+
+                <span class="material-symbols-rounded self-center">
+                    menu
+                </span>
+            </div>
+        </nav>
+    )
+}
