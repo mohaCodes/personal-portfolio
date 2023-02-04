633ff9eb5369040828d37dd6e659b0fca42563ab
Mohamed Abdulmueen <moha.abdulmueen@gmail.com>
2023-02-04 18:50:49 +0300

    file created and done with some code in it

---
+++ src/Components/personalInfo.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Typical from "react-typical";
+
+
+
+export default function PersonalInfo() {
+    return (
+        <main className="p-11">
+
+            <div className="flex flex-col mt-5">
+                <h1 className="font-sofiaSans leading-[2.8rem] text-6xl font-bold tracking-tight">WEB DEVELOPER</h1>
+                <h4 className="font-normal mt-3 text-secondary-white-text text-xs">I'm a <Typical steps={['Full Stack Javascript Developer', 1500, 'Front End Developer', 1500]}loop={Infinity}wrapper="span"/></h4>
+            </div>
+
+        </main>
+    )
+}
