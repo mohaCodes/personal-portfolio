63e7a5d8dcc1b00aa78233a4c02575c619349345
Mohamed Abdulmueen <moha.abdulmueen@gmail.com>
2023-02-04 23:08:11 +0300

    Added main label and react-typical element

--- src/Components/personalInfo.jsx
+++ src/Components/personalInfo.jsx
@@ -1,20 +1,32 @@
-import React from "react";
-import Typical from "react-typical";
-
-
-
+import React from "react"
+import Typical from "react-typical"
 
 export default function PersonalInfo() {
     return (
-        <main className="p-11">
-
+        <main className="px-11 pt-16">
             <div className="flex flex-col mt-5">
-                <h1 className="font-sofiaSans leading-[2.8rem] text-6xl font-bold tracking-tight">WEB DEVELOPER</h1>
-                <h4 className="font-normal mt-3 text-secondary-white-text text-xs">I'm a <Typical steps={['Full Stack Javascript Developer', 1500, 'Front End Developer', 1500, "Android Developer", 1500]}loop={Infinity}wrapper="span"/></h4>
+                <h1 className="font-sofiaSans leading-[2.8rem] text-6xl font-bold tracking-tight">
+                    WEB DEVELOPER
+                </h1>
+                <h4 className="font-normal mt-3 text-white-text text-xs">
+                    I'm a{" "}
+                    <Typical
+                    className="z-0"
+                        steps={[
+                            "Web Developer",
+                            1500,
+                            "Full Stack Javascript Developer",
+                            1500,
+                            "Front End Developer",
+                            1500,
+                        ]}
+                        loop={Infinity}
+                        wrapper="span"
+                    />
+                </h4>
             </div>
 
-            <hr className="mt-16 bg-grayish-white"/>
-            
+            <hr className="mt-12 bg-grayish-white" />
         </main>
     )
 }
