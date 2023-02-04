e851b209dc0e4aaeae0f07beb370d93a2d9fd1db
Mohamed Abdulmueen <moha.abdulmueen@gmail.com>
2023-02-04 12:56:32 +0300

    replaced Header with Nav component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react"
-import Header from "./Components/Header"
+import Nav from "./Components/Nav"
 
 function App() {
     return (
         <div className="">
-            <Header />
-            
+            <Nav />
         </div>
     )
 }
