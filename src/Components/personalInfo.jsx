d7072a93e00f9cab7f98993c904964a3ebb9f03d
Mohamed Abdulmueen <moha.abdulmueen@gmail.com>
2023-02-04 19:26:23 +0300

    added horizontal line and section is doe

--- src/Components/personalInfo.jsx
+++ src/Components/personalInfo.jsx
@@ -3,15 +3,18 @@ import Typical from "react-typical";
 
 
 
+
 export default function PersonalInfo() {
     return (
         <main className="p-11">
 
             <div className="flex flex-col mt-5">
                 <h1 className="font-sofiaSans leading-[2.8rem] text-6xl font-bold tracking-tight">WEB DEVELOPER</h1>
-                <h4 className="font-normal mt-3 text-secondary-white-text text-xs">I'm a <Typical steps={['Full Stack Javascript Developer', 1500, 'Front End Developer', 1500]}loop={Infinity}wrapper="span"/></h4>
+                <h4 className="font-normal mt-3 text-secondary-white-text text-xs">I'm a <Typical steps={['Full Stack Javascript Developer', 1500, 'Front End Developer', 1500, "Android Developer", 1500]}loop={Infinity}wrapper="span"/></h4>
             </div>
 
+            <hr className="mt-16 bg-grayish-white"/>
+            
         </main>
     )
 }
