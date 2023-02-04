26005667d46a54bc4f6058a7ab9323a4f62f0f74
Mohamed Abdulmueen <moha.abdulmueen@gmail.com>
2023-02-04 23:42:16 +0300

    created file and added skills

---
+++ skills.js
@@ -0,0 +1,14 @@
+// this array of objects is being exported to App where it'll be rendered
+
+const skills = [
+    {Javascript: "Javascript", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"},
+    {React: "React", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"},
+    {Html: "HTML5", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"},
+    {Css: "CSS", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"},
+    {Tailwindcss: "Tailwindcss", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg "},
+    {Git: "Git", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"}, 
+    {Kotlin: "Kotlin", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kotlin/kotlin-original.svg"}
+]
+
+
+export default skills
