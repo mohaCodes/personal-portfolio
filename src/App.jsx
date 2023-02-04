d0e5ae122c0ae2a662d6217d68769b012a7b2683
Mohamed Abdulmueen <moha.abdulmueen@gmail.com>
2023-02-04 18:50:07 +0300

    imported PersonalInfo component to display it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Nav from "./Components/Nav"
+import PersonalInfo from "./Components/personalInfo"
 
 function App() {
     return (
         <div className="">
             <Nav />
+            <PersonalInfo/>
         </div>
     )
 }
