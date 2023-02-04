1708a23242afd368161bd2a6a70576418c03ff2c
Mohamed Abdulmueen <moha.abdulmueen@gmail.com>
2023-02-04 23:06:49 +0300

    Added "About" component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import Nav from "./Components/Nav"
 import PersonalInfo from "./Components/personalInfo"
+import About from "./Components/About"
 
 function App() {
     return (
         <div className="">
             <Nav />
-            <PersonalInfo/>
+            <PersonalInfo />
+            <About />
         </div>
     )
 }
