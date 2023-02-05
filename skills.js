c9b7fa537fea84a637f9cb55e897891c4328c6e3
Mohamed Abdulmueen <moha.abdulmueen@gmail.com>
2023-02-06 00:00:23 +0300

    Reformatted array and added more elements

--- skills.js
+++ skills.js
@@ -1,14 +1,38 @@
 // this array of objects is being exported to App where it'll be rendered
 
 const skills = [
-    {Javascript: "Javascript", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"},
-    {React: "React", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg"},
-    {Html: "HTML5", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"},
-    {Css: "CSS", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"},
-    {Tailwindcss: "Tailwindcss", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original-wordmark.svg "},
-    {Git: "Git", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"}, 
-    {Kotlin: "Kotlin", src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kotlin/kotlin-original.svg"}
+    [
+        "React",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg",
+    ],
+    [
+        "Javascript",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+    ],
+    [
+        "HTML",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+    ],
+    [
+        "CSS",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+    ],
+    [
+        "Tailwindcss",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+    ],
+    [
+        "Nodejs",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain.svg"
+    ],
+    [
+        "Kotlin",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kotlin/kotlin-original.svg",
+    ],
+    [
+        "Git",
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+    ],
 ]
-
 
 export default skills
